19cb0eeb5185a1f028a8eb939eee0f4453e3902f
allenhung1025 <hungtungming@gmail.com>
2021-07-11 23:15:59 +0800

    add some description

--- evaluation/NDB_JS/compute_ndb_js.sh
+++ evaluation/NDB_JS/compute_ndb_js.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# modify --gen_dir argument
 python compute_ndb_js.py --mean_std_dir ../../data/looperman --real_dir ./looper_2000 --gen_dir /home/allenhung/nas189/home/stylegan2-pytorch/generated_freesound_one_bar/100000/mel_80_320
