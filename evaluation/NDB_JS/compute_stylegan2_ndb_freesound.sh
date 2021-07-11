2456422767ea641c55f7119c2bc4dcab6e33e5c1
allenhung1025 <hungtungming@gmail.com>
2021-07-11 23:11:57 +0800

    delete

--- evaluation/NDB_JS/compute_stylegan2_ndb_freesound.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python compute_ndb_js.py --mean_std_dir ../../data/looperman --real_dir ./looper_2000 --gen_dir /home/allenhung/nas189/home/stylegan2-pytorch/generated_freesound_one_bar/100000/mel_80_320
