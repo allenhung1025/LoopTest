8c5606e293c3c4b1c99bab0237420da1e62d954c
allenhung1025 <hungtungming@gmail.com>
2021-07-26 21:51:10 +0800

    copmute ndb

--- evaluation/NDB_JS/compute_ndb_js.sh
+++ evaluation/NDB_JS/compute_ndb_js.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# modify --gen_dir argument
-python compute_ndb_js.py --mean_std_dir ../../data/looperman --real_dir ./looper_2000 --gen_dir /home/allenhung/nas189/home/stylegan2-pytorch/generated_freesound_one_bar/100000/mel_80_320
+python compute_ndb_js.py --mean_std_dir ../../data/looperman --real_dir ./looper_2000 --gen_dir ../../generated_freesound_one_bar/100000/mel_80_320
